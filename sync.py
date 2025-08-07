8e25c6fd312b23975064c3c4b5f0a2c8af4d84f6
Venechozix <venechozix@gmail.com>
2025-08-07 14:35:56 -0300

    started developing the functions, stopped using os and instead im using pathlib

--- sync.py
+++ sync.py
@@ -1,23 +1,67 @@
 #!/bin/python3
 
-#This script is in charge of copying stuff 
+# This script is in charge of copying stuff
 
 # -U --> ~/.config -> .../configurations/
-# -S --> .../configurations/ --> ~/.config 
+# -S --> .../configurations/ --> ~/.config
 
 # -N {folder name} --> Add a .config folder to the list
 # -R {folder name} -->  Remove a .config folder from the list
 
 import json
-import os
+from pathlib import Path
+
+# Folders
+CONFIG = "/home/chozix/.config"
+BACKUP = "~/Configurations/"
+
+TEST = "/home/chozix/pyprojects/"
 
 
-folders = []
 
-def copy_folders()
-    #do something
-    pass
+class FileManager:
 
-def add_to_list()
-    #do something
-    pass
+    def printFolders(self, org: str) -> None:
+        p = Path(org)
+ 
+        if not p.exists():
+            print(f"The origin '{org}' does not exist")
+            raise ValueError()
+
+        for folder in p.iterdir():
+            if folder.is_dir():
+                print(folder.name)
+                
+
+    def copy_folders(self, org: str, dst: str):
+        p = Path(org)
+ 
+        if not p.exists():
+            print(f"The origin '{org}' does not exist")
+            raise ValueError()
+        
+            
+
+        # do something
+        pass
+
+
+class ListManager:
+
+    def add_to_list(self, name: str):
+        # do something
+        pass
+
+    def remove_from_list(self, name: str):
+        # do something
+        pass
+
+
+
+fManager = FileManager()
+
+fManager.printFolders(TEST)
+
+
+
+
